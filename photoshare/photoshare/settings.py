db4f380673ec8d61a980a1df526a2812b62e0c06
francois bonnard <francois.bonnard@gmail.com>
2024-03-09 15:34:45 +0100

    debug base_dir

--- photoshare/photoshare/settings.py
+++ photoshare/photoshare/settings.py
@@ -13,8 +13,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+print("____________________________francesco_______________________________________")
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
