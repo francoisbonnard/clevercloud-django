96acba6d4e3c933c920555e4ce3fdf78d565e990
francois bonnard <francois.bonnard@gmail.com>
2024-03-09 21:10:39 +0100

    add photoshare as prefix in installed apps

--- photoshare/photoshare/settings.py
+++ photoshare/photoshare/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photos.apps.PhotosConfig',
+    'photoshare.photos.apps.PhotosConfig',
 ]
 
 MIDDLEWARE = [
