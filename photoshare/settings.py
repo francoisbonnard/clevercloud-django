0843db542cd72f15788e16d9e91423b100c425b7
francois bonnard <francois.bonnard@gmail.com>
2024-03-27 21:29:16 +0100

    move photo folder

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'photos.apps.PhotosConfig',
-    'photoshare.photos.apps.PhotosConfig',
+    'photos.apps.PhotosConfig',
 ]
 
 MIDDLEWARE = [
